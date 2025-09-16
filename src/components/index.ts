e3293acd093985588d1e9a6fb0f9edd21e8da777
Hozaifa <hozaifa4you@gmail.com>
2025-09-16 19:46:17 +0600

    Hero section added

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as HomeSection } from './sections/HeroSection.svelte';
+
+// ui
+export { default as Button } from './ui/Button.svelte';
--- src/components/sections/HeroSection.svelte
+++ src/components/sections/HeroSection.svelte
@@ -1,4 +1,24 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { Button } from '$components';
+
+	function handleClick() {
+		goto('/#contact-form');
+	}
+</script>
+
 <section class="hero-section default-margin">
 	<h3>Hello! I'm Hozaifa</h3>
 	<div class="underscore mb-m"></div>
+
+	<h1>I'm a Svelte Developer crafting intuitive & performance</h1>
+	<h1 class="dark-grey mb-m">web experiences</h1>
+
+	<Button onclick={handleClick}>Let's Talk</Button>
 </section>
+
+<style>
+	.hero-section {
+		padding-top: 60px;
+	}
+</style>
